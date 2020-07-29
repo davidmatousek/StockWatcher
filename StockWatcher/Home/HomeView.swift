9d410dfde88dbca31da6a7f53cd0d87aa3b9a7c7
David Matousek <davidmatousek@me.com>
2020-07-29 00:39:15 -0400

    Added api manager and home view

---
+++ StockWatcher/Home/HomeView.swift
@@ -0,0 +1,21 @@
+//
+//  HomeView.swift
+//  StockWatcher
+//
+//  Created by Matousek, David on 7/29/20.
+//  Copyright © 2020 David Matousek. All rights reserved.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
---
+++ StockWatcher/Managers/IEX_API_Manager.swift
@@ -0,0 +1,9 @@
+//
+//  IEX_API_Manager.swift
+//  StockWatcher
+//
+//  Created by Matousek, David on 7/29/20.
+//  Copyright © 2020 David Matousek. All rights reserved.
+//
+
+import Foundation
